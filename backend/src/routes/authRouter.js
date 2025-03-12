c6bd8dbeb8548683468b276cd0af56837251746a
Aditya Kumbhar <adityakumbhar915@gmail.com>
2025-03-12 11:46:17 +0530

    Project setup compelete

---
+++ backend/src/routes/authRouter.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/signup', (req, res)=>{
+    res.send("signup");
+});
+
+router.get('/login', (req, res)=>{
+    res.send("login");
+});
+
+router.get('/logout', (req, res)=>{
+    res.send("logout");
+});
+
+
+export default router;
